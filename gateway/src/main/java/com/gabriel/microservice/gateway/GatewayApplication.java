6faf84e548505fad6f37985d13b6194261cd5125
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-10-24 08:24:41 -0300

    mudando a dependencia netflix-zuul para spring-cloud-gateway

--- gateway/src/main/java/com/gabriel/microservice/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gabriel/microservice/gateway/GatewayApplication.java
@@ -2,12 +2,8 @@ package com.gabriel.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
