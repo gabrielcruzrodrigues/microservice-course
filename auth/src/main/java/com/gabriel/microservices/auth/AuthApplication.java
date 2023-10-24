142d2fb86ac1372c0c8ecd6bbf5d56a72b34f04e
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-10-24 09:14:47 -0300

    configuração inicial do modulo auth com o modulo core

--- auth/src/main/java/com/gabriel/microservices/auth/AuthApplication.java
+++ auth/src/main/java/com/gabriel/microservices/auth/AuthApplication.java
@@ -1,9 +1,16 @@
 package com.gabriel.microservices.auth;
 
+import org.gabriel.microservices.core.property.JwtConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = JwtConfiguration.class)
+@EntityScan({"org.gabriel.microservices.core.model"})
+@EnableJpaRepositories({"org.gabriel.microservices.core.repository"})
 public class AuthApplication {
 
 	public static void main(String[] args) {
